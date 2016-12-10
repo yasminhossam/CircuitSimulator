de43b846e5c2f41fbe5daffc878ff5bea48438fd
Yasmin Hossam <yasmin_galaxy@hotmail.com>
2016-12-10 14:58:59 +0200

    working with passive elements only

--- src/CircuitSimulator.java
+++ src/CircuitSimulator.java
@@ -13,12 +13,9 @@ public class CircuitSimulator {
 		BufferedReader br = null;
 		FileReader fr = null;
 		String filename = "1.txt";
-		Component[]components = new Component[20];
-		int[] nodes = new int[20];
+		Vector<Component> components = new Vector<Component> ();
 		int m = 0;
-		int i=0;
-		int j=0;
-		Vector<Integer> myVector = new Vector<Integer> ();
+		Vector<Integer> nodes = new Vector<Integer> ();
 
 		try {
 			fr = new FileReader(filename);
@@ -26,36 +23,102 @@ public class CircuitSimulator {
 			String sCurrentLine;
 			br = new BufferedReader(new FileReader(filename));
 			while ((sCurrentLine = br.readLine()) != null) {
-				//System.out.println(sCurrentLine);
 				String[] words = sCurrentLine.split(" ");
 				String type=words[0];
-				if (type=="Vsrc")
+				if (type.equals("Vsrc"))
 					m++;
 				int node1 = Character.getNumericValue(words[1].charAt(1));
 				int node2 = Character.getNumericValue(words[2].charAt(1));
-				myVector.add(node1);
-				myVector.add(node2);
+				nodes.add(node1);
+				nodes.add(node2);
 				double value = Double.parseDouble(words[3]);
 				double initvalue = Double.parseDouble(words[4]);
-				components[i]= new Component(type,node1,node2,value,initvalue);
-				
-				System.out.println(components[i].type+" "+ components[i].node1+ " "+components[i].node2+" "+ components[i].value+ " "+components[i].initial_value);
-				i++;
+				components.add(new Component(type,node1,node2,value,initvalue));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		myVector=new Vector(new HashSet(myVector));
-		int n=myVector.size()-1;
-		for (int k=0; k<myVector.size(); k++){
-			System.out.println(myVector.elementAt(k));
+		
+		for (int i = 0; i < components.size(); i++) {
+			System.out.println(components.elementAt(i).type +" "+ components.elementAt(i).node1 +" "+ components.elementAt(i).node2 +" "+ components.elementAt(i).value +" "+ components.elementAt(i).initial_value);
 		}
+		
+		nodes=new Vector(new HashSet(nodes));
+		int n=nodes.size()-1;
+		
+		double [][] a = new double[m+n][m+n];
+		double[][] z = new double[m+n][1];
+		
+		for (int i = 0; i < m+n; i++) {
+			for (int j = 0; j < m+n; j++) {
+				a[i][j]=0;
+			}
+		}
+		for (int i = 0; i < z.length; i++) {
+			z[i][0]=0;
+		}
+		int k=n;
+		for (int i = 0; i < components.size(); i++) {
+			if (components.elementAt(i).type.equals("R")){
+				if (components.elementAt(i).node2 != 0){
+					a[components.elementAt(i).node1-1][components.elementAt(i).node1-1]+=1/components.elementAt(i).value;
+					a[components.elementAt(i).node1-1][components.elementAt(i).node2-1]-=1/components.elementAt(i).value;
+					a[components.elementAt(i).node2-1][components.elementAt(i).node1-1]-=1/components.elementAt(i).value;
+					a[components.elementAt(i).node2-1][components.elementAt(i).node2-1]+=1/components.elementAt(i).value;
+				}
+				else {
+					a[components.elementAt(i).node1-1][components.elementAt(i).node1-1]+=1/components.elementAt(i).value;
+				}
+			}
+			else if (components.elementAt(i).type.equals("Isrc") ){
+				if (components.elementAt(i).node2 != 0){
+					z[components.elementAt(i).node1-1][0]+=components.elementAt(i).value;
+					z[components.elementAt(i).node2-1][0]-=components.elementAt(i).value;
+				}
+				else {
+					z[components.elementAt(i).node1-1][0]+=components.elementAt(i).value;
+				}
+			}
+			else if (components.elementAt(i).type.equals("Vsrc")){
+				if (components.elementAt(i).node2 != 0){
+					a[components.elementAt(i).node1-1][k]+=1;
+					a[components.elementAt(i).node2-1][k]-=1;
+					a[k][components.elementAt(i).node1-1]+=1;
+					a[k][components.elementAt(i).node2-1]-=1;
+				}
+				else {
+					a[components.elementAt(i).node1-1][k]+=1;
+					a[k][components.elementAt(i).node1-1]+=1;
+				}
+				z[k][0]+=components.elementAt(i).value;
+				k++;
+			}
+		}
+		System.out.println("m:"+m +" n:" +n);
+		for (int i = 0; i < m+n; i++) {
+			for (int j = 0; j < m+n; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+			System.out.println("");
+		}
+		for (int i = 0; i < z.length; i++) {
+			System.out.println(z[i][0]);
+		}
+		Matrix A = new Matrix(a);
+		Matrix Z = new Matrix(z);
+		Matrix x = A.solve(Z);
+		for (int i = 0; i < m+n; i++) {
+			System.out.println(x.get(i, 0));
+		}
+		
+		/*		
 		double[][] array = {{1.,2.,3},{4.,5.,6.},{7.,8.,10.}};
 		Matrix A = new Matrix(array);
 		Matrix b = Matrix.random(3,1);
 		Matrix x = A.solve(b);
 		Matrix Residual = A.times(x).minus(b);
-		double rnorm = Residual.normInf();
+		double rnorm = Residual.normInf();*/
 	}
 
 }
+
--- src/Component.java
+++ src/Component.java
@@ -3,13 +3,13 @@ public class Component {
 	String type;
 	int node1,node2;
 	double initial_value,value;
-	public Component(String type, int node1, int node2, double initial_value, double value) {
+	public Component(String type, int node1, int node2, double value, double initial_value) {
 		super();
 		this.type = type;
 		this.node1 = node1;
 		this.node2 = node2;
-		this.initial_value = initial_value;
 		this.value = value;
+		this.initial_value = initial_value;
 	}
 	
 }
